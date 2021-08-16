ce4197f8f189feedb2afb9d10330d80484603aad
dhaniaryo <dhanipriyono01@gmail.com>
2021-08-16 22:37:42 +0700

    updating component input number

--- src/pages/ExamplePage.js
+++ src/pages/ExamplePage.js
@@ -15,7 +15,7 @@ export default class Example extends Component {
             <div className="container">
                 <div className="row align-items-center justify-content-center" style={{height: "100vh"}}>
                     <div className="col-auto">
-                        <InputNumber max={30} suffix="night" isSuffixPlural onChange={this.handleChange} name="value" value={this.state.value}/>
+                        <InputNumber max={30} suffix=" night" isSuffixPlural onChange={this.handleChange} name="value" value={this.state.value}/>
                     </div>
                 </div>
             </div>
