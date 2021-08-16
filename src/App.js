5ef88ed9389bf255a478edcf683d34b39e78ac0f
dhaniaryo <dhanipriyono01@gmail.com>
2021-08-16 22:34:41 +0700

    adding and styling component input number

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'assets/scss/style.scss';
 import LandingPage from 'pages/LandingPage';
+import Example from 'pages/ExamplePage';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route path="/" component={LandingPage}/>
+        <Route path="/example" component={Example}/>
       </Router>
     </div>
   );
