1d434a5b18b8cc403c55cf8bf403fb3c239fd0d8
dhaniaryo <dhanipriyono01@gmail.com>
2021-08-18 15:22:07 +0700

    adding and styling component breadcrumb

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}/>
+        {/* <Route path="/" component={LandingPage}/> */}
         <Route path="/example" component={Example}/>
       </Router>
     </div>
